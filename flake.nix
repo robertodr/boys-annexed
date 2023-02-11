6c5dadf6f759007d5138cd51f6594171f425803d
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2023-02-11 11:28:36 +0100

    chore: add Nix stuff

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "Code and data repository for Boys' function paper";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    flake-compat,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          cmake
+          gcc
+          hdf5
+          highfive
+          ninja
+
+          (python311.withPackages
+            (ps:
+              with ps; [
+                ps.tqdm
+                ps.h5py
+                ps.jupyterlab
+                ps.numpy
+                ps.plotly
+                ps.scipy
+                ps.sympy
+              ]))
+        ];
+
+        hardeningDisable = ["all"];
+        NINJA_STATUS = "[Built edge %f of %t in %e sec] ";
+      };
+    });
+}
