41b8831f99d7a3a1e19171bf628fee062c20b2a8
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2023-02-17 20:28:29 +0100

    feat: improve run.cpp

--- src/run.cpp
+++ src/run.cpp
@@ -11,26 +11,50 @@ namespace fs = std::filesystem;
 int
 main()
 {
+    constexpr auto max_order = 2;
+
     auto fpath = fs::path("data/boys_reference.h5");
 
-    std::cout << "fpath " << fpath << std::endl;
+    std::cout << "Path to reference data " << fpath << std::endl;
 
-    auto file = H5Easy::File(fpath.string(), H5Easy::File::ReadOnly);
+    auto ref = H5Easy::File(fpath.string(), H5Easy::File::ReadOnly);
 
-    // lo_interval: [0.0, 11.5) region
+    auto cpath = fs::path("data/boys_computed.h5");
+
+    auto computed = H5Easy::File(cpath.string(), H5Easy::File::Overwrite);
+
+    std::cout << "Path to computed data " << cpath << std::endl;
+
+    std::string         lbl, in_lbl, out_lbl;
+    std::vector<double> xs, ys;
+
+    // loop over region: lo, mid, hi
+    for (const auto& region : {"lo", "mid", "hi"})
     {
-        auto xs = H5Easy::load<std::vector<double>>(file, "/lo_interval/xs");
-        auto ys = mcmurchie_davidson::boys_function(0, xs);
-    }
+        lbl = std::string("/") + region + "_interval";
 
-    // mid_interval: [11.5, 13.5) region
-    {
-        auto xs = H5Easy::load<std::vector<double>>(file, "/mid_interval/xs");
-    }
+        in_lbl = lbl + "/xs";
+        xs     = H5Easy::load<std::vector<double>>(ref, in_lbl);
 
-    // hi_interval: [13.5, 150.0) region
-    {
-        auto xs = H5Easy::load<std::vector<double>>(file, "/hi_interval/xs");
+        // loop over method: pade, mcmurchie_davidson
+        // loop over backend: CPU, GPU
+        ys = mcmurchie_davidson::boys_function(max_order, xs);
+
+        auto col = std::vector<double>(xs.size());
+
+        // save order by order
+        for (auto o = 0; o < max_order; ++o)
+        {
+            // collect column
+            for (auto i = 0; i < xs.size(); ++i)
+            {
+                col[i] = ys[o + i * (max_order + 1)];
+            }
+
+            out_lbl = lbl + "/mcmurchie_davidson/cpu/ys/order_" + std::to_string(o);
+
+            H5Easy::dump(computed, out_lbl, col);
+        }
     }
 
     return EXIT_SUCCESS;
