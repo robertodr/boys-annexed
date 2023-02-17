e84b7f9848a986680b63800ea9d257bf570314ac
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2023-02-17 18:34:20 +0100

    chore: regerenate files

--- scripts/generate.py
+++ scripts/generate.py
@@ -8,19 +8,23 @@ import mcmurchie_davidson, pade
 
 
 def main(
-    max_order: int = typer.Argument(..., help="Maximum order of the Boys' function")
+    max_order: int = typer.Argument(..., help="Maximum order of the Boys' function"),
+    folder: Path = typer.Argument(
+        Path("src"), help="Where to store the generated sources."
+    ),
+    overwrite: bool = typer.Option(False, help="Overwrite existing generated files."),
 ) -> None:
-    folder = Path("src/mcmurchie_davidson")
-    folder.mkdir(parents=True)
+    md = folder / "mcmurchie_davidson"
+    md.mkdir(parents=True, exist_ok=overwrite)
     print(f"Generating McMurchie-Davidson table for orders 1 <= O <= {max_order}")
-    mcmurchie_davidson.generate(max_order=max_order, folder=folder)
+    mcmurchie_davidson.generate(max_order=max_order, folder=md)
 
-    folder = Path("src/pade")
-    folder.mkdir(parents=True)
+    pd = folder / "pade"
+    pd.mkdir(parents=True, exist_ok=overwrite)
 
     for P, Q in [(5, 6), (9, 10), (15, 16), (25, 26)]:
         print(f"Generating [{P}/{Q}] Padé approximant for orders 1 <= O <= {max_order}")
-        pade.generate(max_order=max_order, P=P, Q=Q, folder=folder)
+        pade.generate(max_order=max_order, P=P, Q=Q, folder=pd)
 
 
 if __name__ == "__main__":
